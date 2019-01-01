96c953003c8ec10594e23eafa86aabc91513cc33
mgdeguzm <matthew_deguzman@sfu.ca>
2018-12-31 16:27:37 -0800

    added verify_msg -- msg is no longer verified on eris.js

--- eris.js
+++ eris.js
@@ -15,27 +15,33 @@ const eris = new Discord.Client();
  * file
  */
 const token = require("./config/token.json").token;
-const msg_config = require("./config/msg_config.json");
 
 /**
- * Imports tokenizing logic
+ * Imports various models for message parsing
  */
 const tokenize = require("./tokenize/tokenize.js");
+const verify = require("./verify_msg/verify_msg.js");
 
 eris.on("ready", () => {
     console.log(`Eris is currently being used on ${eris.channels.size} servers`);
 })
 
 eris.on("message", async message => {
-    // Ignores messages from other bots -- prevents bot loops
-    if (message.author.bot) return;
+    // // Ignores messages from other bots -- prevents bot loops
+    // if (message.author.bot) return;
 
-    msg = message.content;
+    // msg = message.content;
 
-    // Ignores messages that do not have the predefined prefix
-    if (msg[0] != msg_config.prefix) return;
+    // // Ignores messages that do not have the predefined prefix
+    // if (msg[0] != msg_config.prefix) return;
 
-    command = tokenize.tokenize(msg);   
+    if (!verify.verify(message)) {
+        console.log("bad");
+    }
+    else {
+        console.log("good");
+    }
+    // command = tokenize.tokenize(msg);
 })
 
 eris.login(token);
---
+++ verify_msg/verify_msg.js
@@ -0,0 +1,46 @@
+/**
+ * Author: md
+ * Purpose: Verifies the user's message to see if it
+ * should be parsed and then tokenized
+ */
+
+const msg_prefix = require("../config/msg_prefix.json").prefix;
+
+is_author_human = (msg) => {
+    return !msg.author.bot
+}
+
+is_properly_prefixed = (msg) => {
+    // console.log(`First char: ${string_msg}`);
+    // console.log(`msg_prefix: ${msg_prefix}`);
+    // console.log(`The equality ${string_msg[0] === msg_prefix}`)
+    return msg.content.startsWith(msg_prefix);
+}
+
+verify = (msg) => {
+    /**
+     * Returns true if the message should be
+     * parsed
+     */
+
+    // All our functions from above.
+    const constraints = [is_author_human, is_properly_prefixed];
+
+    // Contains all the booleans returned from applying the functions above into msg.
+    const fn_booleans = [];
+
+    // Takes functions from constraints one by one and applies them to msg.
+    // The resulting boolean is then pushed into fn_booleans.
+    constraints.forEach(fn => {
+        fn_boolean = fn(msg);
+        fn_booleans.push(fn_boolean);
+    });
+
+    // If any of the functions in constraints returns false, then should_be_parsed becomes
+    // false
+    const should_be_parsed = !fn_booleans.some(bool => bool === false);
+
+    return should_be_parsed;
+}
+
+module.exports = {verify};
